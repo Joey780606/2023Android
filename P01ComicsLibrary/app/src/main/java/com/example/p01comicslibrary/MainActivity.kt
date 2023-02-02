0ff14fe21caa5604b96e92cafd0fed1ac265a760
joeyyang <joeyyang@pcpartner.com>
2023-02-02 13:43:11 +0800

    Feat: Join P01ComicsLibrary app
    Solution:
    1. As title, Initial project.
    
    Finder: Joey
    Author: Joey

---
+++ P01ComicsLibrary/app/src/main/java/com/example/p01comicslibrary/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.p01comicslibrary
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.p01comicslibrary.ui.theme.P01ComicsLibraryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            P01ComicsLibraryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    P01ComicsLibraryTheme {
+        Greeting("Android")
+    }
+}
