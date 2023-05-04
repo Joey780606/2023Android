9853ad619d6144bb19cc9eac78fef14661740b2c
joeyyang <joeyyang@pcpartner.com>
2023-05-04 08:46:56 +0800

    Feat: Check unique username
    Solution:
    1. As title. Modify UI, create UserData class, modify viewModel.
    2. Class: Chap. 14
    
    Finder: Joey
    Author: Joey

--- P02Instagram/app/src/main/java/com/example/p02instgram/IgViewModel.kt
+++ P02Instagram/app/src/main/java/com/example/p02instgram/IgViewModel.kt
@@ -1,11 +1,15 @@
 package com.example.p02instgram
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.p02instgram.data.UserData
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+
+const val USERS = "users"
 
 @HiltViewModel
 class IgViewModel @Inject constructor(
@@ -13,4 +17,37 @@ class IgViewModel @Inject constructor(
     val db: FirebaseFirestore,
     val storage: FirebaseStorage
 ): ViewModel() {
+    val signedIn = mutableStateOf(false)
+    val inProgress = mutableStateOf(false)
+    val userData = mutableStateOf<UserData?>(null)
+
+    fun onSignup(username: String, email: String, pass: String) {
+        inProgress.value = true
+
+        db.collection(USERS).whereEqualTo("username", username).get()
+            .addOnSuccessListener { document ->
+                if( document.size() > 0) {
+                    handleException(customMessage = "Username already exists")
+                    inProgress.value = false
+                } else {
+                    auth.createUserWithEmailAndPassword(email, pass).
+                    addOnCompleteListener { task ->
+                        if(task.isSuccessful) {
+                            signedIn.value = true
+                        } else {
+                            handleException(task.exception, "Signup failed")
+                        }
+                        inProgress.value = false
+                    }
+
+                }
+            }
+            .addOnFailureListener {
+
+            }
+    }
+
+    fun handleException(exception: Exception? = null, customMessage: String? = "") {
+
+    }
 }
