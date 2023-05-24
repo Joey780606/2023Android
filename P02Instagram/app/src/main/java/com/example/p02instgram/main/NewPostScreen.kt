af8c26ca5a494b9961a57779855223bf394aba4d
joeyyang <joeyyang@pcpartner.com>
2023-05-24 08:40:34 +0800

    Feat: New post screen UI
    Solution:
    1. As title.
    2. Class: Chap. 32
    
    Finder: Joey
    Author: Joey

--- P02Instagram/app/src/main/java/com/example/p02instgram/main/NewPostScreen.kt
+++ P02Instagram/app/src/main/java/com/example/p02instgram/main/NewPostScreen.kt
@@ -1,10 +1,89 @@
 package com.example.p02instgram.main
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
+
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.example.p02instgram.IgViewModel
 
 @Composable
 fun NewPostScreen(navController: NavController, vm: IgViewModel, encodeUri: String) {
+    val imageUri by remember { mutableStateOf(encodeUri) } //remember 要加 androidx.compose.runtime.getValue
+    var description by rememberSaveable { mutableStateOf("") } //rememberSaveable 要加 androidx.compose.runtime.setValue
+    val scrollState = rememberScrollState()
+    val focusManager = LocalFocusManager.current
 
+    Column(modifier = Modifier
+        .verticalScroll(scrollState)
+        .fillMaxWidth()) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "Cancel", modifier = Modifier.clickable { navController.popBackStack() })
+            Text(text = "Post", modifier = Modifier.clickable {
+                focusManager.clearFocus()
+                //Call the VM
+            })
+        }
+
+        CommonDivider()
+
+        Image(
+            painter = rememberImagePainter(imageUri),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .defaultMinSize(minHeight = 150.dp),    //這是最後修改的
+            contentScale = ContentScale.FillWidth
+        )
+        
+        Row(modifier = Modifier.padding(16.dp)) {
+            OutlinedTextField(
+                value = description,
+                onValueChange = { description = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                label = { Text(text = "Description") },
+                singleLine = false,
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    textColor = Color.Black
+                )
+            )
+        }
+    }
+
+    val inProgress = vm.inProgress.value
+    if(inProgress)
+        CommonProgressSpinner()
 }
