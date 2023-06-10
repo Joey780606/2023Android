1105ecb3d9557111db149e10ef5eb5405e14cfe2
Joey <yty0606@gmail.com>
2023-06-10 15:50:23 +0800

    Feat: Create comment UI
    Solution:
    1. As title.
    2. Class: Chap. 50
    
    Finder: Joey
    Author: Joey

--- P02Instagram/app/src/main/java/com/example/p02instgram/main/CommentsScreen.kt
+++ P02Instagram/app/src/main/java/com/example/p02instgram/main/CommentsScreen.kt
@@ -1,11 +1,66 @@
 package com.example.p02instgram.main
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.navigation.NavController
 import com.example.p02instgram.IgViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CommentsScreen(navController: NavController, vm: IgViewModel, postId: String) {
-    Text(text = "Comments screen")
+    var commentText by rememberSaveable { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = "Comments")
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            TextField(
+                value = commentText,
+                onValueChange = { commentText = it },
+                modifier = Modifier
+                    .weight(1f)
+                    .border(1.dp, Color.LightGray),
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    textColor = Color.Black,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent
+                )
+            )
+            Button(onClick = {
+                vm.createComment(postId = postId, text = commentText)
+                commentText = ""
+                focusManager.clearFocus()
+            },
+                modifier = Modifier.padding(start = 8.dp)
+            ) {
+                Text(text = "Comment")
+            }
+        }
+    }
+
 }
