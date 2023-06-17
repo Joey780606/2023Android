1e5abd99f50c58020c0535836ef5ba39c408ab10
Joey <yty0606@gmail.com>
2023-06-17 12:45:37 +0800

    Feat P01: Character detail screen
    Solution:
    1. As title. 
    4. Class: Chap. 38
    
    Finder: Joey
    Author: Joey

--- P01ComicsLibrary/app/src/main/java/com/example/p01comicslibrary/MainActivity.kt
+++ P01ComicsLibrary/app/src/main/java/com/example/p01comicslibrary/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.p01comicslibrary
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -8,12 +9,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.p01comicslibrary.ui.theme.P01ComicsLibraryTheme
+import com.example.p01comicslibrary.view.CharacterDefaultScreen
 import com.example.p01comicslibrary.view.CharactersBottomNav
 import com.example.p01comicslibrary.view.CollectionScreen
 import com.example.p01comicslibrary.view.LibraryScreen
@@ -23,7 +26,7 @@ import dagger.hilt.android.AndroidEntryPoint
 sealed class Destination(val route: String) {
     object Library: Destination("library")
     object Collection: Destination("collection")
-    object CharacterDetail: Destination("character/{characterId") {
+    object CharacterDetail: Destination("character/{characterId}") {
         fun createRoute(characterId: Int?) = "character/$characterId"
     }
 }
@@ -48,6 +51,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CharactersScaffold(navController: NavHostController, lvm: LibraryApiViewModel) {
     val scaffoldState = rememberScaffoldState()
+    val ctx = LocalContext.current
+
     Scaffold(
         scaffoldState = scaffoldState,
         bottomBar = { CharactersBottomNav(navController = navController)}
@@ -60,6 +65,13 @@ fun CharactersScaffold(navController: NavHostController, lvm: LibraryApiViewMode
                 CollectionScreen()
             }
             composable(Destination.CharacterDetail.route) { navBackStackEntry ->
+                val id = navBackStackEntry.arguments?.getString("characterId")?.toIntOrNull()
+                if (id == null)
+                    Toast.makeText(ctx, "Character id is required", Toast.LENGTH_SHORT).show()
+                else {
+                    lvm.retrieveSingleCharacter(id)
+                    CharacterDefaultScreen(lvm = lvm, paddingValues = paddingValues, navController = navController)
+                }
             }
         }
     }
--- P01ComicsLibrary/app/src/main/java/com/example/p01comicslibrary/view/CharacterDefaultScreen.kt
+++ P01ComicsLibrary/app/src/main/java/com/example/p01comicslibrary/view/CharacterDefaultScreen.kt
@@ -1,9 +1,85 @@
 package com.example.p01comicslibrary.view
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.p01comicslibrary.CharacterImage
+import com.example.p01comicslibrary.Destination
+import com.example.p01comicslibrary.comicsToString
+import com.example.p01comicslibrary.viewmodel.LibraryApiViewModel
 
 @Composable
-fun CharacterDefaultScreen() {
-    Text(text = "CharacterDefaultScreen screen")
+fun CharacterDefaultScreen(
+    lvm: LibraryApiViewModel,
+    paddingValues: PaddingValues,
+    navController: NavController
+) {
+    val character = lvm.characterDetails.value
+
+    if(character == null) {
+        navController.navigate(Destination.Library.route) {
+            popUpTo(Destination.Library.route)
+            launchSingleTop = true
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(4.dp)
+            .padding(bottom = paddingValues.calculateBottomPadding())
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val imageUrl = character?.thumbnail?.path + "," + character?.thumbnail?.extension
+        val title = character?.name?: "No name"
+        val comics = character?.comics?.items?.mapNotNull { it.name }?.comicsToString() ?: "No comics"
+        val description = character?.description ?: "No description"
+
+        CharacterImage(
+            url = imageUrl, modifier = Modifier
+                .width(200.dp)
+                .padding(4.dp)
+        )
+
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            modifier = Modifier.padding(4.dp)
+        )
+
+        Text(
+            text = comics,
+            fontStyle = FontStyle.Italic,
+            fontSize = 12.sp,
+            modifier = Modifier.padding(4.dp)
+        )
+
+        Text(text = description, fontSize = 16.sp, modifier = Modifier.padding(4.dp))
+
+        Button(onClick = {}, modifier = Modifier.padding(bottom = 20.dp)) {
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Icon(Icons.Default.Add, contentDescription = null)
+                Text(text = "Add to collection")
+            }
+        }
+
+    }
 }
