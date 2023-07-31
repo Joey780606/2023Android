d3cce0a7474d32564253208c831b457cc579c7a1
Joey <yty0606@gmail.com>
2023-07-31 09:20:22 +0800

    Feat P01: NoteDao
    Solution:
    1. As title. 
    2. Class: Chap. 45
    
    Finder: Joey
    Author: Joey

---
+++ P01ComicsLibrary/app/src/main/java/com/example/p01comicslibrary/model/db/NoteDao.kt
@@ -0,0 +1,25 @@
+package com.example.p01comicslibrary.model.db
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+    @Query("SELECT * FROM ${Constants.NOTE_TABLE} ORDER BY id")
+    fun getAllNotes(): Flow<List<DbNote>>
+
+    @Query("SELECT * FROM ${Constants.NOTE_TABLE} WHERE characterId = :characterId ORDER BY id ASC")
+    fun getNotes(characterId: Int): Flow<List<DbNote>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun addNote(note: DbNote)
+
+    @Update
+    fun updateNote(note: DbNote)
+
+    @Delete
+    fun deleteNote(note: DbNote)
+
+    @Query("DELETE FROM ${Constants.NOTE_TABLE} WHERE characterId = :characterId")
+    fun deleteAllNotes(characterId: Int)
+}
